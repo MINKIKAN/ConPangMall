5adb4e06bc15bf7eb2324ad459e7a67128043153
SangKwonNo <yhlo000@naver.com>
2023-04-06 11:09:40 +0900

    sad
    asd

--- src/main/java/com/console/mall/controller/Homecontroller.java
+++ src/main/java/com/console/mall/controller/Homecontroller.java
@@ -2,6 +2,7 @@ package com.console.mall.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +12,8 @@ public class Homecontroller {
     @RequestMapping("/")
     public String home(){
         log.info("home controller");
-        return "home";
+        return "index";
     }
+
+    
 }
