71627d30d993cb28d6885009c0d74b81b09b6b59
MINKIKAN <alsrl568@gmail.com>
2023-04-08 18:56:17 +0900

    html
    html

--- src/main/java/com/console/mall/controller/MemberForm.java
+++ src/main/java/com/console/mall/controller/MemberForm.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotEmpty;
 @Getter
 @Setter
 public class MemberForm {
-    @NotEmpty(message = "회원 이름은 필수 있ㅂ니다")
+    @NotEmpty(message = "회원 이름은 필수 있니다")
     private String name;
     private String city;
     private String street;
