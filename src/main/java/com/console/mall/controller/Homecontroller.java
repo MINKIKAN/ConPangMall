ddf895312720be76253b8e6036aff5c74535c7af
SangKwonNo <yhlo000@naver.com>
2023-04-10 13:08:18 +0900

    wqewqe
    sasdsadsa

--- src/main/java/com/console/mall/controller/Homecontroller.java
+++ src/main/java/com/console/mall/controller/Homecontroller.java
@@ -4,13 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @Slf4j
 public class Homecontroller {
 
     @RequestMapping("/")
-    public String home(){
+    public String home(HttpServletRequest request){
         log.info("home controller");
+
         return "home";
     }
 
