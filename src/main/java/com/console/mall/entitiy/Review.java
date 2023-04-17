75472d44f559dc4755380da903f7cb02c2faf1a5
SangKwonNo <yhlo000@naver.com>
2023-04-17 10:16:21 +0900

    Revert "asd"
    This reverts commit 5454c5b52133d1132a3adf04158171c8972a5cef.

--- src/main/java/com/console/mall/entitiy/Review.java
+++ src/main/java/com/console/mall/entitiy/Review.java
@@ -10,21 +10,22 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class Review extends Board {
+public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "review_id")
     private Long id;
 
-
+    private String writer;
     private String image;
-    private int rating;
+    private String title;
 
+    @Column(name = "created_date", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdDate;
 
-
-
-
+    @Column(columnDefinition = "LONGTEXT")
+    private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
